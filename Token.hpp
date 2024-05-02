55feeaa876f2dc0f8bbf5e4bde791d9964c47e5e
Aly Elashram <alyahelashram@gmail.com>
2024-05-02 19:50:17 +0300

    Revert "Use a templated member variable as a generic object as you might get strings or doubles"
    This reverts commit c1f5ca8bc61e28c3fc6d0712ab12a0c64bb1251c.

--- Token.hpp
+++ Token.hpp
@@ -7,31 +7,34 @@
 #include <optional>
 
 
-template<class T>
-class Token {
 
+class Token {
+	
 	TokenType mTokenType;
 
-	// Stores the actual source code string of the expr
-	// Operator or string or class or whatever
-	std::optional<std::string> mLexeme;
+	 // Lexeme is only used for operators and strings
+	 std::optional<std::string> lexeme;
 
-	// Stores the actual value of the expression 
-	// Double Value or String or whatever
-	std::optional<T> mliteral;
-	int mLine;
+	 // If identifier type then it has an identifier name
+	 std::optional<std::string> mIdentifierStr;
 
-public:
-	Token() = default;
-	Token(TokenType type, const int mLine);
-	Token(TokenType type, const std::optional<double> numVal, const int mLine);
-	Token(TokenType type, const std::optional<std::string> identStr, const int mLine);
+	 // If number then it's value is here
+	 std::optional<double> mNumberVal;
+	 
+	 int mLine;
 
-	// Constructor for operators and lexemes
-	Token(const TokenType type, const std::optional<char> lexeme, const int mLine);
-	inline  std::optional<double>& getVal() { return this->mNumberVal; };
-	inline int getLine() { return this->mLine; };
-	inline TokenType getType() { return this->mTokenType; };
-	friend std::ostream& operator<< (std::ostream& os, Token& obj);
-};
+	public:	
+		Token() = default;
+		Token(TokenType type, const int mLine);
+		Token(TokenType type,const std::optional<double> numVal, const int mLine);
+		Token(TokenType type, const std::optional<std::string> identStr, const int mLine);
+
+		// Constructor for operators and lexemes
+		Token(const TokenType type, const std::optional<char> lexeme, const int mLine);
+		inline  std::optional<std::string>& getName() { return this->mIdentifierStr;};
+		inline  std::optional<double>& getVal() { return this->mNumberVal; };
+		inline int getLine() { return this->mLine; };
+		inline TokenType getType() { return this->mTokenType; };
+		friend std::ostream& operator<< (std::ostream& os, Token& obj);
+}; 
 #endif // !token
