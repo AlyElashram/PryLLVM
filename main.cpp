fe312b2fad24d20ba22675998c8c37a9b555e4c2
Aly Elashram <alyahelashram@gmail.com>
2024-08-05 01:36:19 +0300

    Add Compiler Class to print IR

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include "Scanner.hpp"
-#include "ErrorReporter.hpp"
 #include "Parser.hpp"
+#include "Expr.hpp"
 #include <iostream>
+#include "Compiler.hpp"
 
 int main(int argc,char* argv []) {
-	static ErrorReporter reporter = ErrorReporter() ;
 	if (argv[1]) {
 		std::string path = argv[1];
-		Scanner scanner = Scanner(reporter);
+		Scanner scanner = Scanner();
 		scanner.scan(path);
-		Parser parser = Parser(scanner.getTokenVector(), reporter);
-		auto ast = parser.parseExpression();
-		std::cout << scanner;
+		Parser parser = Parser(scanner.getTokenVector());
+		parser.mainLoop();
+		Compiler::getInstance().printIR();
+		// std::cout << scanner;
 	}
 	else {
 		std::cout << "Invalid usage no path supplied";
 	}
 
-	return 0;
+ 	return 0;
 }
